2a83dc675edac0de46af59e162b03070f60e4a77
Germanov Vladimir <vlaveger@gmail.com>
2022-01-29 11:14:52 +0300

    Установка и настройка фреймворка

---
+++ backend/assets/AppAsset.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace backend\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main backend application asset bundle.
+ */
+class AppAsset extends AssetBundle
+{
+    /**
+     * @var string
+     */
+    public $basePath = '@webroot';
+
+    /**
+     * @var string
+     */
+    public $baseUrl = '@web';
+
+    /**
+     * @var string[]
+     */
+    public $css = [
+        'css/site.css',
+    ];
+
+    /**
+     * @var string[]
+     */
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap4\BootstrapAsset',
+    ];
+}
